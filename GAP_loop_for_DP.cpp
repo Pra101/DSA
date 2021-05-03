0563eea862922e4860bff66c87d9ecaacad5d673
Prahalad Singh <62698216+Pra101@users.noreply.github.com>
2021-05-04 04:46:43 +0530

    Create GAP_loop_for_DP.cpp
    My Fast Code for GAP loop

---
+++ GAP_loop_for_DP.cpp
@@ -0,0 +1,14 @@
+// gap loop very handy in 2D Dynamic Programming Problems.
+for(int gap=1;gap<n;gap++){
+		for(int i=0,j=gap;i+gap<n;i++,j++){
+		// NOW, (i,j) can be handled as simple iterators for getting to different positions.
+		}
+	}
+
+// Square loop for DP
+
+for(int i=0;i<n;i++){
+  for(int j=0;j<n;j++){
+    // NOW, (i,j) can be handled as simple iterators for getting to different positions.   
+  }
+}
