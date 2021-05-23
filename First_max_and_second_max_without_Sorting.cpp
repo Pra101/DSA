f4ae9cc6ecad195209a380eb4af8d8673b8f751f
Prahalad Singh <62698216+Pra101@users.noreply.github.com>
2021-05-23 21:15:31 +0530

    Update First_max_and_second_max_without_Sorting.cpp

--- First_max_and_second_max_without_Sorting.cpp
+++ First_max_and_second_max_without_Sorting.cpp
@@ -1,22 +1,17 @@
 // How to find first max and second max without Sorting 
-// Important algorithm
-class Solution {
-public:
-    int maxProduct(vector<int>& nums) {
-        const int n =nums.size();
-        int mx1=0,mx2=1;
-        if(nums[mx1]<nums[mx2])swap(mx1,mx2);
-        for(int i=2;i<n;i++){
-            if(nums[i]>nums[mx1]){
-                int tmp_=mx1;
-                mx1=i;
-                mx2=tmp_;
-                
-            }
-            else if(nums[i]>nums[mx2]){
-                mx2=i;
-            }
+// Important algorithmic:
+vecrtor<int> maxProduct(vector<int>& nums) {
+    const int n =nums.size();
+    int mx1=0,mx2=1;// random distripution
+    
+    if(nums[mx1]<nums[mx2])swap(mx1,mx2);// Just handling first two indexes
+                                         // and ensuring that mx1 has a greater index;
+    for(int i=2;i<n;i++){
+        if(nums[i]>nums[mx1]){
+            swap(mx1,mx2);// give mx1 to mx2
+            mx1=nums[i];  // and give index of greatest element to mx1
         }
-        return (nums[mx1]-1)*(nums[mx2]-1);
+        else if(nums[i]>nums[mx2]) mx2=i; // just give index of second greatest element to mx2;
     }
-};
+    return {(nums[mx1]),(nums[mx2])}; //returned the valuses
+}
