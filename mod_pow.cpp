e4ecdabf9c2d4be31364a5f93e3c3baf91ce83db
Prahalad Singh <62698216+Pra101@users.noreply.github.com>
2022-01-12 14:58:41 +0530

    Update mod_pow.cpp

--- mod_pow.cpp
+++ mod_pow.cpp
@@ -1,9 +1,22 @@
-// for k^n : working fast code: Learn::
+// for k^n : working fast code:
+
+// Recursive 
 int pow2(int k, int n){
     if(n==0)return 1;
     if(n==1)return k;
     int ans=pow2(k, n/2);
     ans=mul(ans,ans);
     if(n%2)ans=mul(ans,k);
+    return ans;
+}
+
+// Iterative
+int power(int x,int y){
+    int ans=1;
+    while(y){
+        if (y&1) ans=mul(ans,x);
+        x=mul(x,x);
+        y>>=1;
+    }
     return ans;
 }
