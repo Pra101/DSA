1630f9948bce8aebaa40848af9db1f667a6469e0
Prahalad Singh <62698216+Pra101@users.noreply.github.com>
2022-01-12 15:23:56 +0530

    Create binary_search_upper_bound.cpp

---
+++ binary_search_upper_bound.cpp
@@ -0,0 +1,21 @@
+// Binary search lower bounds 
+// Works perfectly
+while(1){
+  	int data;
+	  cin>>data;
+	  if(data>arr[n-1]){
+	        cout<<"Values does not exist!!\n";
+	        continue;
+	  }
+	  int l=0, r=n-1,mid;
+	  int ans=n-1;
+	  while(l<=r){
+	      mid=l+(r-l)/2;
+	      if(data<=arr[mid]){
+		          if(mid<ans)ans=mid;
+		          r=mid-1;
+	      }
+	  else l=mid+1;
+    }
+	  cout<<arr[ans]<<"\n";
+}
