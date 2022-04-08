ca78916a3449c19ac07bae500ed83581d176ed00
Prahalad Singh <62698216+Pra101@users.noreply.github.com>
2022-04-08 13:01:17 +0530

    Create KMP_LPS.cpp

---
+++ KMP_LPS.cpp
@@ -0,0 +1,231 @@
+// Author: pra101
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <sys/time.h>
+using namespace std;
+#define int long long
+#define float long double
+#define ll long long
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int,int> pii;
+typedef priority_queue<int> max_heap;
+typedef priority_queue<int, vector<int>, greater<int>> min_heap;
+#define pb push_back
+#define d0(x) cout<<(x)<<" "
+#define d1(x) cout<<(x)<<"\n"
+#define d2(x,y) cout<<(x)<<" "<<(y)<<"\n"
+#define fio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define all(v) (v).begin(),(v).end()
+#define sz(v) (v).size()
+#define enter(a) for (auto &it : (a)) cin >> it
+unsigned int Z;
+void srand_() {
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	Z = (tv.tv_sec ^ tv.tv_usec) | 1;
+} 
+int rand_() {
+    srand_();
+    return (Z *= 3) >> 1;
+}
+///////////////////////////////////////////////For debugging//////////////////////////////////////////////////
+#define watch(x) cout<<(#x)<<" is "<<(x)<<"\n";
+#define TRACE
+#ifdef TRACE
+    #define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+    template <typename Arg1>
+    void __f(const char* name, Arg1&& arg1){
+        cerr << name << " : " << arg1 << endl;
+    }
+    template <typename Arg1, typename... Args>
+    void __f(const char* names, Arg1&& arg1, Args&&... args){
+        const char* comma = strchr(names + 1, ',');
+        cerr.write(names, comma - names) << " : " << arg1<<" | ";
+        __f(comma+1, args...);
+    }
+#else
+    #define trace(...) 1
+#endif
+//==================================================
+template<class T> ostream& operator<<(ostream &os, vector<T> V) {
+    os << "[ ";
+    for(auto v : V) os << v << " ";
+    return os << "]";
+}
+template<class T> ostream& operator<<(ostream &os, set<T> S){
+    os << "{ ";
+    for(auto s:S) os<<s<<" ";
+    return os<<"}";
+}
+template<class T> ostream& operator<<(ostream &os, unordered_set<T> S){
+    os << "{ ";
+    for(auto s:S) os<<s<<" ";
+    return os<<"}";
+}
+template<class L, class R> ostream& operator<<(ostream &os, pair<L,R> P) {
+    return os << "(" << P.first << "," << P.second << ")";
+}
+template<class L, class R> ostream& operator<<(ostream &os, map<L,R> M) {
+    os << "{ ";
+    for(auto m:M) os<<"("<<m.first<<":"<<m.second<<") ";
+    return os<<"}";
+}
+template<class L, class R> ostream& operator<<(ostream &os, unordered_map<L,R> M) {
+    os << "{ ";
+    for(auto m:M) os<<"("<<m.first<<":"<<m.second<<") ";
+    return os<<"}";
+}
+//-----------------------------------------------------------------------------------------------------------
+struct custom_hash { // Credits: https://codeforces.com/blog/entry/62393
+    static uint64_t splitmix64(uint64_t x) { // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+    template<typename L, typename R>
+    size_t operator()(pair<L,R> const& Y) const{
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(Y.first * 31ull + Y.second + FIXED_RANDOM);
+    }
+};
+/*/----------------------------------------Modular Arithmetic---------------------------------------/*/
+const int mod=1e9+7;// 998244353
+inline int add(int x,int y){
+    x+=y;
+    if (x>=mod) return x-mod;
+    return x;
+}
+inline int sub(int x,int y){
+    x-=y;
+    if (x<0) return x+mod;
+    return x;
+}
+inline int mul(int x,int y){
+    return (x*1ll*y)%mod;
+}
+inline int power(int x,int y){
+    int ans=1;
+    while(y){
+        if (y&1) ans=mul(ans,x);
+        x=mul(x,x);
+        y>>=1;
+    }
+    return ans;
+}
+inline int inv(int x){
+    return power(x,mod-2);
+}
+//------------------------------------------------------------------------------------------------------------
+const int maxn=1e6+100;
+const int INF=1e16+7;
+const int MXI = 2e5+5;  
+const float PI = acos((float)-1);
+const int xd[4] = {1,0,-1,0}, yd[4] = {0,1,0,-1};
+//_-
+
+// Fills lps[] for given patttern pat[0..M-1]
+void computeLPSArray(string pat, int M, vector<int>& lps)
+{
+	// length of the previous longest prefix suffix
+	int len = 0;
+
+	lps[0] = 0; // lps[0] is always 0
+
+	// the loop calculates lps[i] for i = 1 to M-1
+	int i = 1;
+	while (i < M) {
+		if (pat[i] == pat[len]) {
+			len++;
+			lps[i] = len;
+			i++;
+		}
+		else // (pat[i] != pat[len])
+		{
+			// This is tricky. Consider the example.
+			// AAACAAAA and i = 7. The idea is similar
+			// to search step.
+			if (len != 0) {
+				len = lps[len - 1];
+
+				// Also, note that we do not increment
+				// i here
+			}
+			else // if (len == 0)
+			{
+				lps[i] = 0;
+				i++;
+			}
+		}
+	}
+}
+
+// Prints occurrences of txt[] in pat[]
+void KMPSearch(string pat, string txt)
+{
+	int M = pat.size();
+	int N = txt.size();
+
+	// create lps[] that will hold the longest prefix suffix
+	// values for pattern
+	vector<int> lps(M);
+
+	// Preprocess the pattern (calculate lps[] array)
+	computeLPSArray(pat, M, lps);
+
+	int i = 0; // index for txt[]
+	int j = 0; // index for pat[]
+	int ans=0;
+	while (i < N) {
+		if (pat[j] == txt[i]) {
+			j++;
+			i++;
+		}
+
+		if (j == M) {
+			//printf("Found pattern at index %d ", i - j);
+			ans++;
+			j= lps[j - 1];
+		}
+
+		// mismatch after j matches
+		else if (i < N && pat[j] != txt[i]) {
+			// Do not match lps[0..lps[j-1]] characters,
+			// they will match anyway
+			if (j != 0)
+				j = lps[j - 1];
+			else
+				i = i + 1;
+		}
+	}
+	cout<<ans<<"\n";
+}
+
+
+int cas=0;
+void Mamba_Mentality(){
+    cout<<"Case "<<cas<<": ";
+    string pat,txt;
+    cin>>pat>>txt;
+    KMPSearch(txt,pat);
+    return ;
+}
+int32_t main(){
+    fio;
+    //#ifndef ONLINE_JUDGE
+        //freopen("in.txt","r",stdin);
+        //freopen("out.txt","w",stdout);
+    //#endif
+    int t=1;
+    cin>>t;
+    while(t--){
+	cas++;
+	Mamba_Mentality();
+    }
+    return 0;
+}
