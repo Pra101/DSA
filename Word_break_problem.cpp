d920db3cb09d2bbfd34b2b4d2d218e9408b8d568
Prahalad Singh <62698216+Pra101@users.noreply.github.com>
2022-07-12 12:38:58 +0530

    Create Word_break_problem.cpp
    Given a string A and a dictionary of words B, determine if A can be segmented into a space-separated sequence of one or more dictionary words.

---
+++ Word_break_problem.cpp
@@ -0,0 +1,50 @@
+//Given a string A and a dictionary of words B, determine if A can be segmented into a space-separated sequence of one or more dictionary words.
+
+// word break memo (Top-Down) 
+unordered_set<string> us;
+string s;
+int N;
+int dp[6501];
+int recur(int idx){
+    if(idx==N)return 1;
+    if(dp[idx]!=-1)return dp[idx];
+    int ans=0;
+    string s_tp="";
+    for(int i=0;i<20;i++){
+        s_tp+=s[idx+i];
+        if(us.count(s_tp)>0){
+            ans|=recur(idx+i+1);
+        }
+        if(ans)break;
+    }
+    return dp[idx]=ans;
+}
+int Solution::wordBreak(string A, vector<string> &B) {
+    us.clear();
+    for(auto ss : B)us.insert(ss);
+    s=A;
+    N=A.size();
+    memset(dp,-1,sizeof(dp));
+    return recur(0);
+}
+// word break Tabular(Bottom-up)
+int Solution::wordBreak(string s, vector<string> &B) {
+    unordered_set<string> us;
+    for(auto ss : B)us.insert(ss);
+    int N=s.size();
+    vector<int> dp(N+1,0);
+    dp[N]=1;
+    for(int idx=N-1;idx>=0;idx--){
+        int ans=0;
+        string s_tp="";
+        for(int i=0;i<20;i++){
+            s_tp+=s[idx+i];
+            if(us.count(s_tp)>0){
+                ans|=dp[idx+i+1];
+            }
+            if(ans)break;
+        }
+        dp[idx]=ans;
+    }
+    return dp[0];
+}
