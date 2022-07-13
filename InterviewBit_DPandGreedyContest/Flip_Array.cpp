4604ee0da9a59a9c2b6aadab3cb2e9222d3251e5
Prahalad Singh <62698216+Pra101@users.noreply.github.com>
2022-07-13 16:20:54 +0530

    Create Flip_Array.cpp

---
+++ InterviewBit_DPandGreedyContest/Flip_Array.cpp
@@ -0,0 +1,40 @@
+const int inf = 10001;
+vector<int> arr;
+int N;
+int s;
+int dp[101][20003];
+// this recurisive function finds the minimum sign flips
+// invoked in second when s has already been find by rec2 runs in quardractic time
+int recur(int idx,int sum){
+    if(idx==N){
+        return ((sum==s)?0:inf);
+    }
+    if(dp[idx][sum+10001]!=-1)return dp[idx][sum+10001];
+    int ans=inf;
+    ans=min(ans,recur(idx+1,sum+arr[idx]));
+    ans=min(ans,recur(idx+1,sum-arr[idx])+1);
+    return dp[idx][sum+10001]=ans;
+}
+// invoked first
+// this recurisive funtions finds the minimum sum in quadractic time
+void rec2(int idx,int sum){
+    if(idx==N){
+        if(sum>=0)s=min(s,sum);
+        return;
+    }
+    if(dp[idx][sum+10001]!=-1)return;
+    rec2(idx+1,sum+arr[idx]);
+    rec2(idx+1,sum-arr[idx]);
+    dp[idx][sum+10001]=1;
+}
+
+int Solution::solve(const vector<int> &A) {
+    arr=A;
+    N=A.size();
+    s=inf;
+    memset(dp,-1,sizeof(dp));
+    rec2(0,0);
+    //cout<<s<<":";
+    memset(dp,-1,sizeof(dp));
+    return recur(0,0);
+}
